2c737635ca759d439d0362a762957a105dadb328
Kunal Soni <kunal.kpt72@gmail.com>
2021-03-21 06:47:56 +0530

    Health Care full AR Part of the Project

---
+++ health/Assets/scripts/main/ChangeScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void btn_changes_scenes(string scene_name){
+        SceneManager.LoadScene(scene_name);
+    }
+}
---
+++ health/Assets/scripts/timerscript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timerscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Image timerBar;
+    public float maxTime = 20f;
+    float timeLeft;
+    public GameObject timeUpText;
+    public GameObject songs;
+
+    void Start()
+    {
+        timeUpText.SetActive (false);
+        timerBar = GetComponent<Image> ();
+        timeLeft = maxTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeLeft > 0) {
+            timeLeft -= Time.deltaTime;
+            timerBar.fillAmount = timeLeft / maxTime;
+        } else{
+            timeUpText.SetActive (true);
+            Time.timeScale = 0;
+        }
+    }
+}
